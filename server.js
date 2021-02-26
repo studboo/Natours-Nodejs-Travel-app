8534f06f41f1d31e2b24b611bf3d50e71fcc2522
studboo <palak3844@gmail.com>
2021-02-26 16:41:02 +0530

    New changes

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose
 
 //---------------------------------------
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
 	console.log(`App listening on ${port}`);
