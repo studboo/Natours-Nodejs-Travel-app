8f21a45301ec7b51a5931b0f4f6a4a2cc6e41184
studboo <palak3844@gmail.com>
2021-02-27 16:43:21 +0530

    new changes

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.post('/webhook-checkout', express.raw({ type: 'application/json' }), bookingController.createBookingCheckout);
+app.post('/webhook-checkout', express.raw({ type: 'application/json' }), bookingController.webhookCheckout);
 
 // Body Parser, reading data form the req.body
 app.use(express.json({ limit: '10kb' }));
